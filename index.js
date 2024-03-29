348637c732d8ac98de4bbe52828bf10f294ff494
PanabeeWQW <timofei.davydo@mail.ru>
2024-03-29 12:33:02 +0300

    add a currencyconv func

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-let a = "2 commit"
+const CC = require('currency-converter-lt')
+let currencyConverter = new CC({from:"USD", to:"JPY", amount:100})
+currencyConverter.convert().then((response) => {
+    console.log(response) //or do something else
+})
