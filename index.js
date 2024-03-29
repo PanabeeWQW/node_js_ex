4caceb4d0b234a148b96674974d097d445687cbf
PanabeeWQW <timofei.davydo@mail.ru>
2024-03-29 12:39:11 +0300

    Revert "add a currencyconv func"
    This reverts commit 348637c732d8ac98de4bbe52828bf10f294ff494.

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-const CC = require('currency-converter-lt')
-let currencyConverter = new CC({from:"USD", to:"JPY", amount:100})
-currencyConverter.convert().then((response) => {
-    console.log(response) //or do something else
-})
+let a = "2 commit"
